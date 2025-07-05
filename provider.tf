50767e112501a65bd2131ee8d27dc660037fb24b
shobhit <redhatshobhit@gmail.com>
2025-07-05 12:38:18 +0530

    Initial commit

---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.26.0"
+    }
+  }
+   backend "azurerm" {
+    resource_group_name = "Terraform_DevOps"  
+    storage_account_name = "tfbackend202504"                              
+    container_name       = "B-17_G20"                                 
+    key                  = "Lab-00.terraform.tfstate"                   
+  }
+}
+
+
+provider "azurerm" {
+    features {   
+    }
+  subscription_id = "cfd26fdd-6d24-43b8-ae23-773b34f779a3"
+}
+
